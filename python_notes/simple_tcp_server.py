c7f5c03731115e92d58d77ff5c480c9f95d06ab4
Ayan Ali <ayanali.sayed2006@gmail.com>
2025-06-20 16:13:55 +0530

    Create simple_tcp_server.py

---
+++ python_notes/simple_tcp_server.py
@@ -0,0 +1,12 @@
+import socket
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind((127.0.0.1 , 80))
+s.listen(1)
+print("server is listening....")
+client, addr = s.accept()
+print(f"connected by {addr}")
+client.send(b"wellcome client")
+data = client.recv(1024)
+print("received:", data.decode())
+client.close()
+s.close()
